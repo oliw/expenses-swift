47d906d0aab4c23012fe780b68701fa26298fe99
Oliver Wilkie <oliverw@squareup.com>
2016-08-07 20:10:29 -0700

    Add custom nav item

--- Expenses/PaymentsViewController.swift
+++ Expenses/PaymentsViewController.swift
@@ -13,14 +13,25 @@ class PaymentsViewController: UITableViewController {
     var events:[Event] = eventsData
     var payments:[Payment] = paymentsData
 
+    @IBOutlet weak var newPaymentButton: UIBarButtonItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.parentViewController?.navigationItem.rightBarButtonItem = self.newPaymentButton
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.parentViewController?.navigationItem.rightBarButtonItem = nil
     }
 
     override func didReceiveMemoryWarning() {
