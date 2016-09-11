745e6f23ac61c116a5cbd6197517b0001cd94fa3
Oliver Wilkie <oliverw@squareup.com>
2016-09-11 16:09:25 -0700

    Minor UI

--- Expenses/ChoosePeopleViewController.swift
+++ Expenses/ChoosePeopleViewController.swift
@@ -15,6 +15,8 @@ class ChoosePeopleViewController: UITableViewController {
     
     var checkedPeople = [Person]()
     
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,6 +24,7 @@ class ChoosePeopleViewController: UITableViewController {
         expense?.getParticipants().forEach({person -> Void in
             checkedPeople.append(person)
         })
+        checkedPeopleChanged()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -30,6 +33,17 @@ class ChoosePeopleViewController: UITableViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    
+    func checkedPeopleChanged() {
+        if checkedPeople.isEmpty {
+            doneButton.enabled = false
+            doneButton.tintColor = UIColor.clearColor()
+        } else {
+            doneButton.enabled = true
+            doneButton.tintColor = nil
+        }
     }
 
     // MARK: - Table view data source
@@ -63,43 +77,9 @@ class ChoosePeopleViewController: UITableViewController {
         } else {
             checkedPeople.append(person)
         }
+        checkedPeopleChanged()
         tableView.reloadData()
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
     
     // MARK: - Action
     
